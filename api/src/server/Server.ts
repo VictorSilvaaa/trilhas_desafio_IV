d48c881252a30262d659e87622bb3edc0ddde0b6
Aik0o1 <anesrotciv@gmail.com>
2024-06-10 18:08:17 -0300

    Configurando middlewares para o express

--- api/src/server/Server.ts
+++ api/src/server/Server.ts
@@ -1,6 +1,12 @@
 import  express from 'express';
+import bodyParser from 'body-parser';
+import cors from "cors";
 
 const server = express();
+
+// adicionando middlewares
+server.use(bodyParser.json());
+server.use(cors());
 
 //server.method('router',function)
 server.get('/', (req, res)=>{
