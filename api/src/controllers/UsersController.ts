7ebb25991418bd143364f52be1bc714726b534ef
Marco Teixeira <marcoteixeira.m47t@gmail.com>
2024-06-21 23:46:08 -0300

    Update UsersController.ts
    implementando update

--- api/src/controllers/UsersController.ts
+++ api/src/controllers/UsersController.ts
@@ -29,8 +29,49 @@ class UsersController {
     }
 
     async update(request: FastifyRequest, reply: FastifyReply) {
-        //atualizar dados do usuario
-    }
+        // Define o schema de validação para os dados de atualização
+        const updateUserSchema = z.object({
+          id: z.string().uuid(),
+          name: z.string().min(3).max(50),
+          email: z.string().email(),
+          password: z.string().min(8).max(50)
+        }).partial(); // Permite atualizações parciais
+    
+        try {
+          const { id, ...updateData } = updateUserSchema.parse(request.body);
+    
+        
+          const existingUser = await prismaClient.user.findUnique({
+            where: { id },
+          });
+    
+          if (!existingUser) {
+            return reply.status(404).send({ message: 'Usuário não encontrado' });
+          }
+    
+          // atualiza os dados 
+          const updatedUser = await prismaClient.user.update({
+            where: { id },
+            data: updateData,
+          });
+    
+          return reply.status(200).send({ message: 'Usuário atualizado com sucesso', user: updatedUser });
+    
+        } catch (error: any) {
+          const errorJson = JSON.stringify(error);
+    
+          // erro de validação
+          if (error instanceof z.ZodError) {
+            return reply.status(400).send(errorJson);
+          } else if (error instanceof prismaClient.PrismaClientKnownRequestError && error.code === 'P2025') {
+            // erro específico do Prisma
+            return reply.status(404).send({ message: 'Usuário não encontrado' });
+          } else {
+            return reply.status(500).send(errorJson);
+          }
+        }
+      }
+
 
     async delete(request: FastifyRequest, reply: FastifyReply) {
         //deletar usuario
