58e3b757a7f3b122f217fb83890208e0c2e18a4a
João Victor da Silva Sales <56794256+VictorSilvaaa@users.noreply.github.com>
2024-06-23 11:20:32 -0300

    refact: corrige usersRepository

--- api/src/controllers/UsersController.ts
+++ api/src/controllers/UsersController.ts
@@ -1,6 +1,7 @@
 import { FastifyRequest, FastifyReply } from 'fastify';
 import prismaClient from "../prisma";
 import { z } from "zod";
+import { UsersRepository } from "../repositories/UsersRepository";
 
 class UsersController {
     async store(request: FastifyRequest, reply: FastifyReply) {
@@ -10,11 +11,16 @@ class UsersController {
             password: z.string().min(8).max(50)
         });
         try{
-            const user = userSchema.parse(request.body);
+            let userData = userSchema.parse(request.body);
             
-            //salva os dados no banco
+            const usersRepository = new UsersRepository();
+            const user = await usersRepository.createUser(userData);
         
-            return user
+            return reply.send({
+              success: true,
+              message: "Usuario cadastrado com sucesso",
+              data: user
+            }).status(200);
 
         } catch (error) {
             const errorJson = JSON.stringify(error)
@@ -29,52 +35,12 @@ class UsersController {
     }
 
     async update(request: FastifyRequest, reply: FastifyReply) {
-        // Define o schema de validação para os dados de atualização
-        const updateUserSchema = z.object({
-          id: z.string().uuid(),
-          name: z.string().min(3).max(50),
-          email: z.string().email(),
-          password: z.string().min(8).max(50)
-        }).partial(); // Permite atualizações parciais
-    
-        try {
-          const { id, ...updateData } = updateUserSchema.parse(request.body);
-    
-        
-          const existingUser = await prismaClient.user.findUnique({
-            where: { id },
-          });
-    
-          if (!existingUser) {
-            return reply.status(404).send({ message: 'Usuário não encontrado' });
-          }
-    
-          // atualiza os dados 
-          const updatedUser = await prismaClient.user.update({
-            where: { id },
-            data: updateData,
-          });
-    
-          return reply.status(200).send({ message: 'Usuário atualizado com sucesso', user: updatedUser });
-    
-        } catch (error: any) {
-          const errorJson = JSON.stringify(error);
-    
-          // erro de validação
-          if (error instanceof z.ZodError) {
-            return reply.status(400).send(errorJson);
-          } else if (error instanceof prismaClient.PrismaClientKnownRequestError && error.code === 'P2025') {
-            // erro específico do Prisma
-            return reply.status(404).send({ message: 'Usuário não encontrado' });
-          } else {
-            return reply.status(500).send(errorJson);
-          }
-        }
-      }
+        //logica de atualizar usuario
+    }
 
 
     async delete(request: FastifyRequest, reply: FastifyReply) {
-        //deletar usuario
+        //logica de deletar usuario
     }
 
 }
--- api/src/repositories/UsersRepository.ts
+++ api/src/repositories/UsersRepository.ts
@@ -1,51 +1,53 @@
 // src/repositories/UsersRepository.js
 
 import { SignPrivateKeyInput } from "crypto";
+import { z } from "zod";
+import { PrismaClient } from '@prisma/client';
 
-const { PrismaClient } = require('@prisma/client');
-
-const prisma = new PrismaClient();
-
+interface User {
+  name: string,
+  email: string,
+  password: string
+}
 class UsersRepository {
-  async createUser(username:string, email:string, password:string) {
-    const user = await prisma.user.create({
-      data: {
-        username,
-        email,
-        password,
+
+  protected prisma = new PrismaClient();
+
+  async createUser(userData: User) {
+    const user = await this.prisma.user.create({data:userData});
+    return user;
+  }
+
+  async getUserById(userId:number) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: `${userId}`,
       },
     });
     return user;
   }
 
-  async getUserById(id:number) {
-    const user = await prisma.user.findUnique({
-      where: { id },
-    });
-    return user;
-  }
-
   async getUserByEmail(email:string) {
-    const user = await prisma.user.findUnique({
-      where: { email },
+    const user = await this.prisma.user.findUnique({
+      where: { email:email },
     });
     return user;
   }
 
-  async updateUser(id:number, newData:object) {
-    const user = await prisma.user.update({
-      where: { id },
+  async updateUser(id:number, newData:User) {
+    const user = await this.prisma.user.update({
+      where: { id: `${id}` },
       data: newData,
     });
     return user;
   }
 
   async deleteUser(id:number) {
-    const user = await prisma.user.delete({
-      where: { id },
+    const user = await this.prisma.user.delete({
+      where: { id: `${id}` },
     });
     return user;
   }
 }
 
-module.exports = new UsersRepository();
+export { UsersRepository };
